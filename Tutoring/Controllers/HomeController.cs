7e4b81cd1523b096a0184d739b117d062b5a59e7
Ben White <bwhite2140@outlook.com>
2020-12-21 09:59:28 -0800

    initialize http client

--- Tutoring/Controllers/HomeController.cs
+++ Tutoring/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Tutoring.Models;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace Tutoring.Controllers
 {
   public class HomeController : Controller
   {
+    static HttpClient client = new HttpClient();
     public ActionResult Index()
     {
+      return View();
+    }
+    public async Task<ActionResult> Search()
+    {
+      client.BaseAddress = new Uri("https://api.scryfall.com/cards/search");
+      client.DefaultRequestHeaders.Accept.Clear();
+      client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/json"));
       return View();
     }
   }
