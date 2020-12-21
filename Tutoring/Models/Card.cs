72d984a7bc5d890217c7ff71f724ab6189c85a56
Ben White <bwhite2140@outlook.com>
2020-12-21 09:17:40 -0800

    add properties to card

--- Tutoring/Models/Card.cs
+++ Tutoring/Models/Card.cs
@@ -1,7 +1,21 @@
+using System;
+
 namespace Tutoring.Models
 {
   public class Card
   {
-    
+    public string Id { get; set; }
+    public Array CardFaces { get; set; }
+    public decimal Cmc { get; set; }
+    public string ManaCost { get; set; }
+    public Array Colors { get; set; }
+    public Array ColorIdentity { get; set; }
+    public Array Keywords { get; set; }
+    public string Layout { get; set; }
+    public string Name { get; set; }
+    public string TypeLine { get; set; }
+    public string Power { get; set; }
+    public string Toughness { get; set; }
+    public string Rarity { get; set; }
   }
 }
