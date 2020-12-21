c4e44d6aac861740017b7e140fcaebd5a37bafb3
Ben White <bwhite2140@outlook.com>
2020-12-21 08:41:41 -0800

    add properties to card list

--- Tutoring/Models/CardList.cs
+++ Tutoring/Models/CardList.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace Tutoring.Models
 {
   public class CardList
   {
-    
+    public Array Data { get; set; }
+    public bool HasMore { get; set; }
+    public Uri PageNext { get; set; }
+    public int TotalCards { get; set; }
   }
 }
