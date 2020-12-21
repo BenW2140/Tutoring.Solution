8eea012d26c3251258ca11b8912b2750c80103cf
Ben White <bwhite2140@outlook.com>
2020-12-21 10:43:21 -0800

    add a get request to search for cards

--- Tutoring/Controllers/HomeController.cs
+++ Tutoring/Controllers/HomeController.cs
@@ -15,12 +15,18 @@ namespace Tutoring.Controllers
     {
       return View();
     }
-    public async Task<ActionResult> Search()
+    public async Task<ActionResult> Search(string parameter)
     {
       client.BaseAddress = new Uri("https://api.scryfall.com/cards/search");
       client.DefaultRequestHeaders.Accept.Clear();
       client.DefaultRequestHeaders.Accept.Add(
         new MediaTypeWithQualityHeaderValue("application/json"));
+      CardList cardList = null;
+      HttpResponseMessage response = await client.GetAsync(parameter);
+      if(response.IsSuccessStatusCode)
+      {
+        cardList = await response.Content.ReadAsAsync<CardList>();
+      }
       return View();
     }
   }
