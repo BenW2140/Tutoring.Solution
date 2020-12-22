78f19368ee5ec7fa1df083118fa7d82dd91fef0f
Ben White <bwhite2140@outlook.com>
2020-12-22 15:44:08 -0800

    move search code back to home controller

--- Tutoring/Controllers/HomeController.cs
+++ Tutoring/Controllers/HomeController.cs
@@ -1,17 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Tutoring.Models;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace Tutoring.Controllers
 {
   public class HomeController : Controller
   {
+    static HttpClient client = new HttpClient();
     public ActionResult Index()
     {
       return View();
     }
-    public ActionResult Display(string parameter)
+    public async Task<ActionResult> Search(string parameter)
     {
-      CardList cardList = cardList.Search(parameter);
+      client.BaseAddress = new Uri("https://api.scryfall.com/cards/search?q=");
+      client.DefaultRequestHeaders.Accept.Clear();
+      client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/json"));
+      CardList cardList = null;
+      HttpResponseMessage response = await client.GetAsync(parameter);
+      if(response.IsSuccessStatusCode)
+      {
+        cardList = await response.Content.ReadAsAsync<CardList>();
+      }
       return View(cardList);
     }
   }
