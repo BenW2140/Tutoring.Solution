c0dfea8c48597cb21cd12facc6e03d4340d9c825
Ben White <bwhite2140@outlook.com>
2020-12-22 12:56:40 -0800

    move api call into cardlist model

--- Tutoring/Controllers/HomeController.cs
+++ Tutoring/Controllers/HomeController.cs
@@ -1,33 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
-using Tutoring.Models;
-using System;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 
 namespace Tutoring.Controllers
 {
   public class HomeController : Controller
   {
-    static HttpClient client = new HttpClient();
     public ActionResult Index()
     {
       return View();
     }
-    public async Task<ActionResult> Search(string parameter)
+    public ActionResult Display()
     {
-      client.BaseAddress = new Uri("https://api.scryfall.com/cards/search");
-      client.DefaultRequestHeaders.Accept.Clear();
-      client.DefaultRequestHeaders.Accept.Add(
-        new MediaTypeWithQualityHeaderValue("application/json"));
-      CardList cardList = null;
-      HttpResponseMessage response = await client.GetAsync(parameter);
-      if(response.IsSuccessStatusCode)
-      {
-        cardList = await response.Content.ReadAsAsync<CardList>();
-      }
-      return View(cardList);
+      
+      return View();
     }
   }
 }
--- Tutoring/Models/CardList.cs
+++ Tutoring/Models/CardList.cs
@@ -1,12 +1,33 @@
+using Tutoring.Models;
 using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace Tutoring.Models
 {
   public class CardList
   {
+    static HttpClient client = new HttpClient();
     public Array Data { get; set; }
     public bool HasMore { get; set; }
     public Uri PageNext { get; set; }
     public int TotalCards { get; set; }
+
+    public async Task<CardList> Search(string parameter)
+    {
+      client.BaseAddress = new Uri("https://api.scryfall.com/cards/search");
+      client.DefaultRequestHeaders.Accept.Clear();
+      client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/json"));
+      CardList cardList = null;
+      HttpResponseMessage response = await client.GetAsync(parameter);
+      if(response.IsSuccessStatusCode)
+      {
+        cardList = await response.Content.ReadAsAsync<CardList>();
+      }
+      return cardList;
+    }
   }
 }
